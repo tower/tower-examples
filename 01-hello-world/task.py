9cf0a5418f36f059675317616bcb6417df94792b
Brad Heller <brad@tower.dev>
2024-11-20 17:56:21 +0100

    Updates to demonstrate parameters

--- 01-hello-world/task.py
+++ 01-hello-world/task.py
@@ -1,9 +1,13 @@
+import os
 import time
 
 # This task does nothing at all, really.
 count = 0
 
+friend = os.getenv("friend")
+foe = os.getenv("foe")
+
 while count < 5:
     count += 1
-    print("Hello, world!")
+    print("Hello, {friend}! Boo to {foe}".format(friend=friend, foe=foe))
     time.sleep(1)
